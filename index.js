80a4e47f887ca26b5be868ba3d109246b6065115
Bryan Forbes <bryan@reigndropsfall.net>
2016-08-01 11:17:08 -0500

    Remap coverage directly from `coverage` property instead of using a file watcher

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-var chokidar = require('chokidar');
-var remapIstanbul = require('remap-istanbul');
+/* globals WeakMap, Promise */
 
-function getSourcesCount(sources) {
-  if (Array.isArray(sources)) { return sources.length; }
-  if (typeof sources === 'string') { return 1; }
-
-  return null;
-}
+var istanbul = require('istanbul');
+var remap = require('remap-istanbul/lib/remap');
+var writeReport = require('remap-istanbul/lib/writeReport');
 
 var KarmaRemapIstanbul = function (baseReporterDecorator, logger, config) {
   baseReporterDecorator(this);
@@ -14,79 +10,70 @@ var KarmaRemapIstanbul = function (baseReporterDecorator, logger, config) {
   var log = logger.create('reporter.remap-istanbul');
 
   var remapIstanbulReporterConfig = config.remapIstanbulReporter || {};
-  var sources = remapIstanbulReporterConfig.src || null;
   var reports = remapIstanbulReporterConfig.reports || {};
-  var timeoutNotCreated = remapIstanbulReporterConfig.timeoutNotCreated || 1000;
-  var timeoutNoMoreFiles = remapIstanbulReporterConfig.timeoutNoMoreFiles || 1000;
 
-  var sourcesCount = getSourcesCount(sources);
-  var pendingReport = 0;
+  var coverageMap;
+
+  this.onRunStart = function (browsers) {
+    coverageMap = new WeakMap();
+  };
+
+  this.onBrowserComplete = function (browser, result) {
+    if (!result || !result.coverage) {
+      return;
+    }
+
+    coverageMap.set(browser, result.coverage);
+  };
+
   var reportFinished = function () { };
-  var noMoreFilesTimeout;
 
-  this.onRunComplete = function (browser) {
-    if (!sources) return;
+  this.onRunComplete = function (browsers) {
+    // Collect the unmapped coverage information for all browsers in this run
+    var unmappedCoverage = (function () {
+      var collector = new istanbul.Collector();
 
-    pendingReport++;
-    var addedPaths = 0;
+      browsers.forEach(function (browser) {
+        var coverage = coverageMap.get(browser);
 
-    // Add watcher for source files
-    var watcher = chokidar.watch(sources, {
-      awaitWriteFinish: {
-        stabilityThreshold: 500,
-        pollInterval: 100
-      }
-    }).on('add', function (path) {
-      addedPaths++;
-      clearTimeout(noMoreFilesTimeout);
+        if (!coverage) {
+          return;
+        }
 
-      if (addedPaths >= sourcesCount) {
-        remap(watcher);
-      } else {
-        noMoreFilesTimeout = setTimeout(function () {
-          log.warn('Not all files specified in sources could be found, continue with partial remapping.');
-          remap(watcher);
-        }, timeoutNoMoreFiles);
-      }
-    });
+        collector.add(coverage);
+      });
 
-    // Check if no file is found after "timeoutNotCreated", close watcher and exit with
-    // a warning
-    setTimeout(function () {
-      if (addedPaths === 0) {
-        pendingReport--;
-        watcher.close();
-        log.warn('Could not find any specified files, exiting without doing anything.');
-        reportFinished();
-      }
-    }, timeoutNotCreated);
+      return collector.getFinalCoverage();
+    })();
 
+    var sourceStore = istanbul.Store.create('memory');
+    var collector = remap(unmappedCoverage, { sources: sourceStore });
+
+    Promise.all(Object.keys(reports).map(function (reportType) {
+      var destination = reports[reportType];
+
+      log.debug('Writing coverage to %s', destination);
+
+      return writeReport(collector, reportType, {}, destination, sourceStore);
+    })).catch(function (error) {
+      log.error(error);
+    }).then(function () {
+      collector.dispose();
+      coverageMap = null;
+
+      reportFinished();
+
+      // Reassign `onExit` to just call `done` since all reports have been written
+      this.onExit = function (done) {
+        done();
+      };
+    }.bind(this));
   };
 
   this.onExit = function (done) {
-    if (pendingReport) {
-      reportFinished = done;
-    } else {
-      done();
-    }
+    // Reports have not been written, so assign `done` to `reportFinished`
+    reportFinished = done;
   };
-
-  /**
-   * Close the chokidar file watcher and call remap-istanbul and exit
-   * plugin execution after successfull or erroneous return value from remapIstanbul
-   */
-  function remap(watcher) {
-    pendingReport--;
-    watcher.close();
-
-    remapIstanbul(sources, reports).then(
-      function (response) { reportFinished(); },
-      function (errorResponse) {
-        log.warn(errorResponse);
-        reportFinished();
-      }
-    );
-  }
 };
 
 KarmaRemapIstanbul.$inject = ['baseReporterDecorator', 'logger', 'config'];
