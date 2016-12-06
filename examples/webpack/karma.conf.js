9dfa5f1fa76b5e83e77a4ddd1e96ff0bc58570ab
Matt Lewis <mattlewis92@users.noreply.github.com>
2016-12-06 13:01:24 +0200

    Add webpack e2e example (#22)

---
+++ examples/webpack/karma.conf.js
@@ -0,0 +1,88 @@
+const webpack = require('webpack');
+
+const webpackConfig = {
+  module: {
+    loaders: [{
+      test: /\.ts$/,
+      loader: 'ts-loader',
+      exclude: /node_modules/
+    }],
+    postLoaders: [{
+      test: /src\/.+\.ts$/,
+      exclude: /(node_modules|\.spec\.ts$)/,
+      loader: 'sourcemap-istanbul-instrumenter-loader?force-sourcemap=true'
+    }]
+  },
+  plugins: [
+    new webpack.SourceMapDevToolPlugin({
+      filename: null,
+      test: /\.(ts|js)($|\?)/i
+    })
+  ],
+  resolve: {
+    extensions: ['', '.ts', '.js']
+  }
+};
+
+module.exports = config => {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: './',
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['mocha'],
+
+    // list of files / patterns to load in the browser
+    files: [
+      'test/test.spec.ts'
+    ],
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      'test/test.spec.ts': ['webpack', 'sourcemap']
+    },
+
+    webpack: webpackConfig,
+
+    remapIstanbulReporter: {
+      reports: {
+        html: 'coverage/html',
+        'text-summary': null
+      }
+    },
+
+    phantomjsLauncher: {
+      // Have phantomjs exit if a ResourceError is encountered (useful if karma exits without killing phantom)
+      exitOnResourceError: true
+    },
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['dots', 'coverage', 'karma-remap-istanbul'],
+
+    // web server port
+    port: 9876,
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS'],
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true
+  });
+};
